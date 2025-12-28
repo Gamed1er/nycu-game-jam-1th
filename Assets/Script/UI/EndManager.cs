fb176094b542364c250c906efb798cf24b809753
gdnb <90841938+Gamed1er@users.noreply.github.com>
2025-12-28 18:53:16 +0800

    end

--- Assets/Script/UI/EndManager.cs
+++ Assets/Script/UI/EndManager.cs
@@ -5,8 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class EndManager : MonoBehaviour
 {
-    public Image image;
-    public Sprite[] sprites;          // 7 張圖片（Inspector 依序放）
+    public Image[] images;          // 7 張圖片（Inspector 依序放）
     public Button backButton;       // 回首頁按鈕
 
     public float fadeInTime = 1f;   // a
@@ -16,30 +15,34 @@ public class EndManager : MonoBehaviour
     void Start()
     {
         backButton.gameObject.SetActive(false);
-        image.gameObject.SetActive(false);
-        SetAlpha(image, 0);
+
+        foreach (var img in images)
+        {
+            img.gameObject.SetActive(false);
+            SetAlpha(img, 0);
+        }
+
         StartCoroutine(PlayEndSequence());
     }
 
     IEnumerator PlayEndSequence()
     {
-        for (int i = 0; i < sprites.Length; i++)
+        for (int i = 0; i < images.Length; i++)
         {
-            Sprite spr = sprites[i];
-            image.gameObject.SetActive(true);
-            image.sprite = spr;
+            Image img = images[i];
+            img.gameObject.SetActive(true);
 
             // 淡入
-            yield return Fade(image, 0, 1, fadeInTime);
+            yield return Fade(img, 0, 1, fadeInTime);
 
             // 顯示
             yield return new WaitForSeconds(showTime);
 
             // 最後一張不淡出
-            if (i != sprites.Length - 1)
+            if (i != images.Length - 1)
             {
-                yield return Fade(image, 1, 0, fadeOutTime);
-                image.gameObject.SetActive(false);
+                yield return Fade(img, 1, 0, fadeOutTime);
+                img.gameObject.SetActive(false);
             }
         }
 
@@ -70,6 +73,6 @@ public class EndManager : MonoBehaviour
     // 給 Button 用
     public void BackToTitle()
     {
-        SceneManager.LoadScene("Lobby"); // 換成你的首頁場景名
+        SceneManager.LoadScene("Title"); // 換成你的首頁場景名
     }
 }
